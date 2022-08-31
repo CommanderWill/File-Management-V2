0a98faa0eab6ffdb2b094c68218fa2a55368d4bc
Lego Commander Will <LegoCommanderwill@gmail.com>
2022-08-30 21:47:05 -0400

    e

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-File Management V2
+###File Management V2
+
+This is a file management class which is for the primary purpose of being used in projects lead or solely worked on by Lego Commander Will
--- file_management.h
+++ file_management.h
@@ -4,8 +4,17 @@
 
 class FILE_MANAGEMENT {
 public:
-	std::string date;
 
-	void fileNew(std::string filename, std::string filetype, bool include_date);
+	void fileNew(std::string filepath, std::string filename, std::string filetype, bool include_date);
+	void fileDelete(std::string filepath, std::string filename, std::string filetype);
+	void fileMove(std::string currentFilePath, std::string newFilePath, std::string filename, std::string filetype);
+	void fileCopy(std::string currentFilePath, std::string newFilePath, std::string filename, std::string filetype);
 
+	std::string fileRead(std::string filepath, std::string filename, std::string filetype);
+	std::string fileReadLine(std::string filepath, std::string filename, std::string filetype, int line);
+
+	void fileWrite(std::string filepath, std::string filename, std::string filetype, std::string output, bool newline);
+	void fileWrite(std::string filepath, std::string filename, std::string filetype, std::string output, bool newline, std::string output2, bool newline2);
+	void fileWrite(std::string filepath, std::string filename, std::string filetype, std::string output, bool newline, std::string output2, bool newline2, std::string output3, bool newline3);
+	void fileWrite(std::string filepath, std::string filename, std::string filetype, std::string output, bool newline, std::string output2, bool newline2, std::string output3, bool newline3, std::string output4, bool newline4);
 };
